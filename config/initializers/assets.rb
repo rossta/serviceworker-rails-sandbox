3078934c9c503ff64afc94ce2caed1d7ad002884
Ross Kaffenberger <rosskaff@gmail.com>
2016-04-30 14:06:08 -0400

    Make sure to precompile serviceworker scripts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( offline-fallback.js )
+Rails.application.config.assets.precompile += %w(
+  home/serviceworker.js
+  offline-fallback/serviceworker.js
+  offline-fallback.js
+)
