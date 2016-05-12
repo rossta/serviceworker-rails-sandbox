441cb745a89fdc595a6d98e1b66439b06e7d5222
Ross Kaffenberger <rosskaff@gmail.com>
2016-05-12 09:38:30 -0400

    Bump asset version

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
   # Add additional assets to the asset load path
   # Rails.application.config.assets.paths << Emoji.images_path
