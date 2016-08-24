f7e3a222c8e9f984c5c3d3f38cfa6e7ad3133a94
Ross Kaffenberger <rosskaff@gmail.com>
2016-08-24 19:06:11 -0400

    Post rails rails:update changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,16 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+# Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+
+# Rails.application.config.assets.precompile += %w( search.js )
+#
 Rails.application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.1'
@@ -25,3 +38,4 @@ Rails.application.configure do
     "--extension=\".jsx\""
   ]
 end
+
