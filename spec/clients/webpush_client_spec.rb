013ab0e8bedb35c0126a7b8d16f95fc41947166c
Ross Kaffenberger <rosskaff@gmail.com>
2016-10-14 00:18:40 -0400

    Fix webpush client specs

--- spec/clients/webpush_client_spec.rb
+++ spec/clients/webpush_client_spec.rb
@@ -7,14 +7,22 @@ describe WebpushClient do
     it "sends to google endpoint" do
       p256dh = SecureRandom.random_bytes(65)
       auth = SecureRandom.random_bytes(16)
-      endpoint = "https://android.googleapis.com/gcm/send/subscriptionId"
+      endpoint = "https://fcm.googleapis.com/gcm/send/subscriptionId"
+
+      allow(WebpushClient).to receive(:public_key).and_return("public_key")
+      allow(WebpushClient).to receive(:private_key).and_return("private_key")
 
       expect(Webpush).to receive(:payload_send).with(
         message: "Hello World",
-        endpoint: "https://android.googleapis.com/gcm/send/subscriptionId",
+        endpoint: "https://fcm.googleapis.com/gcm/send/subscriptionId",
         p256dh: p256dh,
         auth: auth,
-        api_key: ENV.fetch('GOOGLE_CLOUD_MESSAGE_API_KEY')
+        vapid: {
+          subject: "mailto:ross@rossta.net",
+          public_key: "public_key",
+          private_key: "private_key"
+        }
+
       )
 
       client.send_notification("Hello World", endpoint: endpoint, p256dh: p256dh, auth: auth)
