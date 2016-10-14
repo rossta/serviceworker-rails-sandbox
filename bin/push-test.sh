81f63e133118d0b63b39450283ef901c8f9d0886
Ross Kaffenberger <rosskaff@gmail.com>
2016-10-14 00:13:06 -0400

    Remove unneeded scripts

--- bin/push-test.sh
+++
@@ -1,2 +0,0 @@
-curl --header "Authorization: key=XXXXXXXXXXXX" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\"registration_ids\":[\"fs...Tw:APA...SzXha\"]}"
-
