a6f395973648027407eb17c3d86cab0c0e8b487f
Ross Kaffenberger <rosskaff@gmail.com>
2016-10-15 10:40:16 -0400

    Get material sideNav working

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,10 +1,28 @@
-(function() {
-  function navReady() {
+(function($) {
+
+  // Detect touch screen and enable scrollbar if necessary
+  function is_touch_device() {
+    try {
+      document.createEvent("TouchEvent");
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function ready() {
     // Initialize collapse button
     $(".button-collapse").sideNav();
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
     // $('.collapsible').collapsible();
+
+    $("#nav-mobile ul.collapsible li li.active").parents("li").children("a.collapsible-header").click()
+
+    if (is_touch_device()) {
+      $('#nav-mobile').css({ overflow: 'auto'});
+    }
   }
 
-  $(document).on('ready page:change page:load', navReady);
-})();
+  $(ready); // end of document ready
+
+})(jQuery);
